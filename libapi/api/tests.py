8702ac43e054583a47b34bc55587623580d2f849
TheLe0 <leob.tosin@hotmail.com>
2021-08-10 21:50:16 -0300

    Setup CI and implement tests

--- libapi/api/tests.py
+++ libapi/api/tests.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import Author
 
-# Create your tests here.
+class AuthorTest(TestCase):
+
+    def setUp(self):
+        base_user = User.objects.create_user(
+            'TestUser',
+            email=None,
+            password=None
+        )
+
+        Author.objects.create(
+            name='Leonardo Tosin'
+        )
+
+    def test_find_author(self):
+        author = Author.objects.get(name = 'Leonardo Tosin')
+        self.assertNotEqual(author.id, 0)
