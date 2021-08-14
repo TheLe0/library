addc7224134cdaea68aa1e653eacd0cb7408fb0a
TheLe0 <leob.tosin@hotmail.com>
2021-08-14 20:58:48 -0300

    Add ordering on Models

--- libapi/api/models.py
+++ libapi/api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=35)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
@@ -12,6 +15,9 @@ class Book(models.Model):
     title = models.CharField(max_length=45)
     edition = models.CharField(max_length=15)
     publication_year = models.IntegerField()
+
+    class Meta:
+        ordering = ['publication_year', 'title']
 
     def __str__(self):
         return self.title
