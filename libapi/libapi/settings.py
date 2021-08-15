c9a1ade3958a189c6d2622d8474aa10c6ea1bf78
TheLe0 <leob.tosin@hotmail.com>
2021-08-15 20:38:47 -0300

    Add localhost to allowed hosts

--- libapi/libapi/settings.py
+++ libapi/libapi/settings.py
@@ -20,7 +20,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.28.239.177']
+ALLOWED_HOSTS = [
+    '172.28.239.177',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
